c2f4d873a921094a358890140cd8602a59561fbd
mnamvarcfd <46830884+mnamvarcfd@users.noreply.github.com>
2020-02-23 00:27:49 +0330

    write results

--- Homework 2/src/lbm.cpp
+++ Homework 2/src/lbm.cpp
@@ -61,54 +61,8 @@ void writeVelocityProfile(Lattice lat, Domain domain);
 //	double *omega, int k, double cx, double cy, double c1, double c2, double c3, 
 //	double c4, double c5, double cs, double tau, double *g, double dt);
 //double collisionWall(int iNode, Lattice lat, int k);
-void writeResults(Domain domain, Lattice lat) {
+void writeResults(Domain domain, Lattice lat);
 
-	FILE *file1;
-	fopen_s(&file1, "Results.txt", "w");
-	fprintf(file1, " i  j       f0           ");
-	fprintf(file1, "f1          f2            f3           ");
-	fprintf(file1, "f4             f5            f6           ");
-	fprintf(file1, "f7            f8  \n");
-
-	int iNode = 0;
-	for (int j = 0; j < domain.getNy(); j++) {
-		for (int i = 0; i < domain.getNx(); i++) {
-			iNode = j * domain.getNx() + i;
-
-			fprintf(file1, "%d  %d ", i, j);
-
-
-			for (int k = 0; k < 9; k++) {
-				fprintf(file1, "%.9f  ", lat.f_[iNode][k]);
-			}
-			fprintf(file1, "\n");
-		
-
-			iNode++;
-		}
-	}
-
-
-	fprintf(file1, " i  j          ro           u             v \n ");
-
-	iNode = 0;
-	for (int j = 0; j < domain.getNy(); j++) {
-		for (int i = 0; i < domain.getNx(); i++) {
-			iNode = j * domain.getNx() + i;
-
-			fprintf(file1, "%d  %d ", i, j);
-
-
-			fprintf(file1, "%.9f  ", lat.rho_[iNode]);
-			fprintf(file1, "%.9f  ", lat.u_[iNode][0]);
-			fprintf(file1, "%.9f  ", lat.u_[iNode][1]);
-			fprintf(file1, "  \n ");
-
-			iNode++;
-		}
-	}
-	fclose(file1);
-}
 
 void setXi(double **Xi, double Xi_r)
 {
